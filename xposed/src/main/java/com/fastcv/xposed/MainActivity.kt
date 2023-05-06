ca55474f27f1ef064f815702cc369e40c28c3cb1
肖初律 <xiaochulu@umeox.com>
2023-05-06 11:36:14 +0800

    增加xposed功能代码

---
+++ xposed/src/main/java/com/fastcv/xposed/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fastcv.xposed
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
