ac8d58a0b6d833089daf0f52e195c8f41e099234
肖初律 <xiaochulu@umeox.com>
2023-06-12 17:37:38 +0800

    新增代码

---
+++ GoogleServer/maptest/src/main/java/com/umeox/maptest/MapsActivity.kt
@@ -0,0 +1,93 @@
+package com.umeox.maptest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
+import com.umeox.maptest.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+     binding = ActivityMapsBinding.inflate(layoutInflater)
+     setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+
+        val maxZoomLevel = mMap.maxZoomLevel
+        val minZoomLevel = mMap.minZoomLevel
+        Log.d("xcl_debug", "onMapReady: maxZoomLevel = $maxZoomLevel   minZoomLevel = $minZoomLevel")
+//        mMap.setMinZoomPreference(17f)
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(22.57484, 113.94807)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+
+        val polyline1 = mMap.addPolyline(
+            PolylineOptions()
+            .clickable(true)
+            .add(
+                LatLng(22.57284, 113.92707),
+                LatLng(22.46384, 113.93607),
+                LatLng(22.55484, 113.94507),
+                LatLng(22.54584, 113.55407),
+                LatLng(22.53684, 113.96307),
+                LatLng(22.52784, 113.97207)))
+        // Store a data object with the polyline, used here to indicate an arbitrary type.
+        polyline1.tag = "A"
+        // Style the polyline.
+        stylePolyline(polyline1)
+    }
+
+    private val COLOR_BLACK_ARGB = -0x1000000
+    private val POLYLINE_STROKE_WIDTH_PX = 12
+
+    private fun stylePolyline(polyline: Polyline) {
+        // Get the data object stored with the polyline.
+        val type = polyline.tag?.toString() ?: ""
+        when (type) {
+            "A" -> {
+                // Use a custom bitmap as the cap at the start of the line.
+                polyline.startCap = CustomCap(
+                    BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher), 5f)
+            }
+            "B" -> {
+                // Use a round cap at the start of the line.
+                polyline.startCap = RoundCap()
+            }
+        }
+        polyline.endCap = RoundCap()
+        polyline.width = POLYLINE_STROKE_WIDTH_PX.toFloat()
+        polyline.color = COLOR_BLACK_ARGB
+        polyline.jointType = JointType.ROUND
+    }
+
+}
